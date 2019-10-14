139c0d442cc163951ad5908953fe4638d93f1b7a
dng <dominicnzl@gmail.com>
2019-10-14 17:42:15 +0200

    polishing some minor issues

--- src/main/java/ng/dominic/parser/web/RecordHandler.java
+++ src/main/java/ng/dominic/parser/web/RecordHandler.java
@@ -7,13 +7,11 @@ import ng.dominic.parser.service.RecordServiceXmlImpl;
 import ng.dominic.parser.service.ValidationServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
